b954a7f371f81fc614ddc669108fb1add23762bd
John Andrew <johnandrewsanvictores@gmail.com>
2021-10-20 11:55:02 +0800

    update files

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ const main_container = document.querySelector('.main-container');
 const graph_div = document.querySelector('.graph-div');
 const cancel_graph_btn =  document.querySelector('#graph-cancel');
 
+const rl_inputs = Array.from(document.querySelectorAll('.rl-input'));
+const calc_inputs_btn = document.querySelector('#calculate-inputs')
+const clear_inputs_btn = document.querySelector('#clear-inputs');
+
 const valid_eq_re = /\([xy][\+\-]\d+\)\^2(\s?)+=(\s?)+\-?(\d?)+(\s?)+\([xy][\+\-]\d+\)/
+
 var graph_exist = false;
 var calculator;
 
@@ -82,22 +87,23 @@ const Parabola = function (equation){
         return [focus,directrix,aos,`${lr} unit/s`,elr]
     }
 
-    function convert_num(num){
-        let obj = math.fraction(num);
-        let n = obj.n;
-        let d = obj.d;
-
-        if(d != 1){
-            return `${n}/${d}`
-        }
-        return num
-    }
-
     return {
         getOpening,
         getOtherParts,
         getVertexForm : () => `(${convert_num(h)}, ${convert_num(k)})`,
     }
+}
+
+
+function convert_num(num){
+    let obj = math.fraction(num);
+    let n = obj.n;
+    let d = obj.d;
+
+    if(d != 1){
+        return `${n}/${d}`
+    }
+    return num
 }
 
 
@@ -152,8 +158,75 @@ function hide_graph(){
 }
 
 
+function check_inputs(){
+    const [wide,deep,p,focus] = rl_inputs;
+    const real_life_parabola = real_life_prob(wide,deep,p,focus);
+
+    if(p.value === '' && (wide.value != '' && deep.value != '')){
+        real_life_parabola.setP();
+        real_life_parabola.setFocus();
+    }else if(wide.value === '' && (deep.value != '' && p.value != '')){
+        real_life_parabola.setWide();
+        real_life_parabola.setFocus();
+    }else if(deep.value === '' && (wide.value != '' && p.value != '')){
+        real_life_parabola.setDeep();
+        real_life_parabola.setFocus();
+    }else if(p.value != '' && focus.value ===''){
+        real_life_parabola.setFocus();
+    }
+
+    rl_inputs.map(input => input.disabled = true);
+}
+
+function clear_input(){
+    rl_inputs.map(input => input.value = '');
+    rl_inputs.map(input => input.disabled = false);
+}
+
+
+const real_life_prob = function (wide,deep,p,focus){
+    let right_side;
+    let left_side;
+    let result;
+    let x = parseFloat(wide.value) / 2
+
+    function setP(){
+        right_side = 4 * eval(deep.value);
+        result = x**2 / right_side
+        p.value = convert_num(parseFloat(result));
+    }
+
+    function setFocus(){
+        focus.value = '(0, ' + p.value + ')';
+    }
+
+    function setDeep(){
+        left_side = x**2;
+        right_side = parseFloat(4 * eval(p.value));
+        deep.value = convert_num(left_side/right_side);
+    }
+
+    function setWide(){
+        wide.value = convert_num((Math.sqrt(4*eval(p.value)*eval(deep.value)) * 2));
+    }
+
+    return{
+        setP,
+        setFocus,
+        setWide,
+        setDeep,
+    }
+}
+
+
 clear_btn.addEventListener('click', clear);
 equation.addEventListener('keyup', validate_eq);
 show_graph_btn.addEventListener('click', show_graph);
 cancel_graph_btn.addEventListener('click', hide_graph);
-window.addEventListener('load', validate_eq);
+
+clear_inputs_btn.addEventListener('click', clear_input);
+calc_inputs_btn.addEventListener('click', check_inputs);
+window.addEventListener('load', () => {
+    validate_eq();
+    clear_input();
+});
