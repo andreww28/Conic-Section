2dd9be214e43ca8e535cdc18b8ba33413396355c
John Andrew <johnandrewsanvictores@gmail.com>
2021-10-26 01:22:50 +0800

    update all

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,152 @@
-var x = document.getElementById("myLinks");
-function myFunction() {
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
+
+const Main = function (){
+  var x = document.getElementById("myLinks");
+  const ham_icon = document.querySelector(".icon");
+
+  function show_nav() {
+      if (x.style.display === "flex") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "flex";
+      }
+  } 
+
+  function check_window_size(){
+    if(window.innerWidth < 800){
+      const navLink = Array.from(document.querySelectorAll('.navlink'));
+      navLink.map(link => link.addEventListener('click', () => x.style.display = "none"));
     }
-} 
-if(window.innerWidth < 800){
-  const navLink = Array.from(document.querySelectorAll('.navlink'));
-  navLink.map(link => link.addEventListener('click', () => x.style.display = "none"));
+  }
+
+  function addEvent(){
+    ham_icon.addEventListener('click', show_nav);
+  }
+
+  return {
+    addEvent,
+    check_window_size,
+  }
 }
+
+
+const Shape_part = function(){
+  const shape_selection = document.querySelector('#shape-selection');
+  const shape_options = Array.from(document.querySelectorAll('#shape-selection > option'));
+  const equation_input = document.querySelector('#input-eq');
+  const output_div = document.querySelector('.output-div');
+
+  
+  function check_shape(shape){
+    let equation = equation_input.value;
+
+    switch (shape.toLowerCase()){
+      case 'circle':
+        circle(equation);
+        break;
+
+      case 'parabola':
+        parabola(equation);
+        break;
+
+      case 'ellipse':
+        ellipse(equation);
+        break;
+
+      case 'hyperbola':
+        hyperbola(equation);
+        break;
+    }
+  }
+
+  function set_nodes_value(values){
+    const node_values = document.querySelectorAll('.value')
+    for(let i = 0; i < node_values.length; i++){
+      node_values[i].textContent = values[i];
+    }
+  }
+
+  function validate(bool){
+    if(bool){
+      equation_input.style.border = '3px solid green';
+    }else if(equation_input.value.length === 0){
+      equation_input.style.border = '2px solid var(--dark-bg)';
+    }else{
+      equation_input.style.border = '3px solid red';
+    }
+  }
+  
+
+  function parabola(equation){
+    set_output_nodes(['Directions', 'Focal Length(p)', 'Vertex', 'Focus', 'Directrix', 'Axis of Symmetry', 'Length of Latus Rectum', 'Endpoints of Latus Rectum']);
+    const Parabola_c = new Parabola(equation);
+    const is_valid = Parabola_c.validate();
+
+    validate(is_valid);
+    if(is_valid){
+      Parabola_c.assign_values();
+      set_nodes_value(Parabola_c.get_all_parts_value());
+    }
+  }
+
+
+  function circle(equation){
+    set_output_nodes(['Center', 'Diameter', 'Radius', 'Area', 'Circumference']);
+    console.log('circle');
+  }
+
+
+  function ellipse(equation){}
+
+
+  function hyperbola(equation){}
+
+
+  function set_output_nodes(titles){
+    output_div.innerHTML = '';
+    let nodes = '<p id="legend">Parts:</p>';
+
+    for(let i = 0; i < titles.length; i++){
+      nodes += `
+            <div class="parts">
+                <p class="title">${titles[i]}:</p>
+                <p class="value">undefined</p>
+            </div>
+      `;
+    }
+
+    output_div.innerHTML = nodes;
+  }
+
+
+  function addEvent(){
+    shape_options.map(option => option.addEventListener('click', (event) => {
+      equation_input.value = '';
+      check_shape(event.target.textContent);
+    }));
+    equation_input.addEventListener('keyup', () => {
+      check_shape(shape_selection.value);
+    });
+  }
+  
+  return {
+    addEvent,
+    check_shape
+  }
+};
+
+
+(function (){
+  const shape_selection = document.querySelector('#shape-selection');
+  const main = Main();
+  const shape_part = Shape_part();
+
+  window.addEventListener('load', () => {
+    main.check_window_size();
+    main.addEvent();
+    shape_part.addEvent();
+    shape_part.check_shape(shape_selection.value);
+  });
+})();
 
 
 
--- scripts/parabola.js
+++ scripts/parabola.js
@@ -1,15 +1,26 @@
-const Parabola_parts = function (){
-    const main_parabola = Main(); 
-    const equation = document.querySelector('#equation').value;
+class Parabola{
+    constructor(equation){
+        this.equation = equation;
+        this.valid_eq_re = /^\([xy][\+\-]\d+(\/\d+)?(\.?\d+){0,1}\)\^2(\s?)+=(\s?)+\-?(\d?(\/\d+)?(\.{0,1}\d+){0,1})+(\s?)+\([xy][\+\-]\d+(\/\d+)?(\.?\d+){0,1}\)$/;
+        this.opening = null;
+        this.h = null;
+        this.k = null;
+        this.c = null;
+    }
 
-    let opening = getOpening();
-    let c = eval(getFocalLength());
-    let [h,k] = getVertex();
+    validate(){
+        return this.equation.match(this.valid_eq_re);
+    }
 
-    let focus,directrix,aos,lr,elr;
+    assign_values(){
+        this.opening = this.getOpening();
+        this.h = this.getVertex()[0];
+        this.k = this.getVertex()[1];
+        this.c = eval(this.getFocalLength());
+    }
 
-    function getOpening(){
-        directions = {
+    getOpening(){
+        const directions = {
             Upward:  /\=(\s?)+(\d?)+(\/\d+)?(\.\d+)?\(y/,
             Downward: /\=(\s?)+\-(\d?)+(\/\d+)?(\.\d+)?\(y/,
             "To the right": /\=(\s?)+(\d?)+(\/\d+)?(\.\d+)?\(x/,
@@ -17,15 +28,14 @@ const Parabola_parts = function (){
         };
 
         for(const [key,value] of Object.entries(directions)){
-            let result = equation.match(value);
+            let result = this.equation.match(value);
             if(result){
                 return key;
             }
         }
     }
 
-
-    function getFocalLength(){
+    getFocalLength(){
         const cases = {
             wholeNumber : /\=(\s?)+\-?\d+(\s?)+\(/,
             noNumber : /\=(\s?)+\-?(\s?)+\(/,
@@ -34,285 +44,60 @@ const Parabola_parts = function (){
         }
 
         let c ;
-        if(equation.match(cases.noNumber)){
-            return math.fraction(`${1/4}`);
-        }else if(equation.match(cases.wholeNumber)){
-            c = equation.match(cases.wholeNumber)[0].split(' ')[1].slice(0,-1);
-        }else if(equation.match(cases.fraction)){
-            c = equation.match(cases.fraction)[0].split(' ')[1].slice(0,-1)
+        if(this.equation.match(cases.noNumber)){
+            return 1/4;
+        }else if(this.equation.match(cases.wholeNumber)){
+            c = this.equation.match(cases.wholeNumber)[0].split(' ')[1].slice(0,-1);
+        }else if(this.equation.match(cases.fraction)){
+            c = this.equation.match(cases.fraction)[0].split(' ')[1].slice(0,-1)
             c = eval(c);
-        }else if(equation.match(cases.decimal)){
-            c = equation.match(cases.decimal)[0].split(' ')[1].slice(0,-1)
+        }else if(this.equation.match(cases.decimal)){
+            c = this.equation.match(cases.decimal)[0].split(' ')[1].slice(0,-1)
         }
         return Math.abs(parseFloat(c/4));
     }
 
-
-    function getVertex(){
-
-        const h = equation.match(/x.\d+(\/\d+)?(\.\d+)?/)[0].slice(1);
-        const k = equation.match(/y.\d+(\/\d+)?(\.\d+)?/)[0].slice(1);
+    getVertex(){
+        const h = this.equation.match(/x.\d+(\/\d+)?(\.\d+)?/)[0].slice(1);
+        const k = this.equation.match(/y.\d+(\/\d+)?(\.\d+)?/)[0].slice(1);
 
         return [h,k].map((num) => parseFloat(eval(num)) * -1);
     }
 
+    getOtherParts(){
+        let focus,directrix,aos,lr,elr;
 
-    function getOtherParts(){
-        if(opening == "Upward"){
-            focus = `(${main_parabola.convert_num(h)}, ${main_parabola.convert_num(k + c)})`
-            directrix = `y = ${main_parabola.convert_num(k - c)}`
-            aos = `x = ${main_parabola.convert_num(h)}`
-            lr = 4 * c
-            elr = `[(${main_parabola.convert_num(h + 2*c)}, ${main_parabola.convert_num(k + c)}), (${main_parabola.convert_num(h- 2*c)}, ${main_parabola.convert_num(k + c)})]`
-        }else if(opening == "Downward"){
-            focus = `(${main_parabola.convert_num(h)}, ${main_parabola.convert_num(k - c)})`
-            directrix = `y = ${main_parabola.convert_num(k + c)}`
-            aos = `x = ${main_parabola.convert_num(h)}`
-            lr = 4 * c
-            elr = `[(${main_parabola.convert_num(h + 2*c)}, ${main_parabola.convert_num(k - c)}), (${main_parabola.convert_num(h- 2*c)}, ${main_parabola.convert_num(k - c)})]`
-        }else if(opening == "To the left"){
-            focus = `(${main_parabola.convert_num(h-c)}, ${main_parabola.convert_num(k)})`
-            directrix = `x = ${main_parabola.convert_num(h+c)}`
-            aos = `y = ${main_parabola.convert_num(k)}`
-            lr = 4 * c
-            elr = `[(${main_parabola.convert_num(h-c)}, ${main_parabola.convert_num(k + 2*c)}), (${main_parabola.convert_num(h-c)}, ${main_parabola.convert_num(k - 2*c)})]`
-        }else if(opening == "To the right"){
-            focus = `(${main_parabola.convert_num(h+c)}, ${main_parabola.convert_num(k)})`
-            directrix = `x = ${main_parabola.convert_num(h-c)}`
-            aos = `y = ${main_parabola.convert_num(k)}`
-            lr = 4 * c
-            elr = `[(${main_parabola.convert_num(h+c)}, ${main_parabola.convert_num(k + 2*c)}), (${main_parabola.convert_num(h+c)}, ${main_parabola.convert_num(k - 2*c)})]`
+        if(this.opening == "Upward"){
+            focus = `(${this.h}, ${this.k + this.c})`
+            directrix = `y = ${this.k-this.c}`
+            aos = `x = ${this.h}`
+            lr = 4 * this.c
+            elr = `[(${this.h + 2*this.c}, ${this.k + this.c}), (${this.h- 2*this.c}, ${this.k + this.c})]`
+        }else if(this.opening == "Downward"){
+            focus = `(${this.h}, ${this.k - this.c})`
+            directrix = `y = ${this.k + this.c}`
+            aos = `x = ${this.h}`
+            lr = 4 * this.c
+            elr = `[(${this.h + 2*this.c}, ${this.k - this.c}), (${this.h- 2*this.c}, ${this.k - this.c})]`
+        }else if(this.opening == "To the left"){
+            focus = `(${this.h-this.c}, ${this.k})`
+            directrix = `x = ${this.h+this.c}`
+            aos = `y = ${this.k}`
+            lr = 4 * this.c
+            elr = `[(${this.h-this.c}, ${this.k + 2*this.c}), (${this.h-this.c}, ${this.k - 2*this.c})]`
+        }else if(this.opening == "To the right"){
+            focus = `(${this.h+this.c}, ${this.k})`
+            directrix = `x = ${this.h-this.c}`
+            aos = `y = ${this.k}`
+            lr = 4 * this.c
+            elr = `[(${this.h+this.c}, ${this.k + 2*this.c}), (${this.h+this.c}, ${this.k - 2*this.c})]`
         }
 
         return [focus,directrix,aos,`${lr} unit/s`,elr]
     }
 
-    return {
-        getOpening,
-        getOtherParts,
-        getVertexForm : () => `(${main_parabola.convert_num(h)}, ${main_parabola.convert_num(k)})`,
+    get_all_parts_value(){
+        const parts_values = [this.opening, this.c, `(${this.h}, ${this.k})`, ...this.getOtherParts()];
+        return parts_values;
     }
-};
-
-
-const Main = function(){
-    const graph_div = document.querySelector('.graph-div');
-    const cancel_graph_btn =  document.querySelector('#graph-cancel');
-    const label = Array.from(document.querySelectorAll('.value'));
-    const clear_btn = document.querySelector('#clear');
-    const show_graph_btn = document.querySelector('#graph-btn');
-    const toDecimal_btn = document.querySelector('#to-decimal');
-    const main_container = document.querySelector('.main-container');
-    const valid_eq_re = /\([xy][\+\-]\d+(\/\d+)?(\.?\d+){0,1}\)\^2(\s?)+=(\s?)+\-?(\d?(\/\d+)?(\.{0,1}\d+){0,1})+(\s?)+\([xy][\+\-]\d+(\/\d+)?(\.?\d+){0,1}\)/
-    
-    var graph_exist = false;
-    var calculator;
-
-    function convert_num(num){
-        if(toDecimal_btn.textContent === 'To Decimal'){
-            let obj = math.fraction(num);
-            let n = obj.n;
-            let d = obj.d;
-            console.log(num, n,d);
-        
-            if(d != 1){ 
-                if(num.toString().match('\-')) return `-${n}/${d}`
-                return `${n}/${d}`
-            }
-
-        }else if(toDecimal_btn.textContent === 'To Fraction'){
-            if(num.toString().match('\.')){
-                return Math.round(num * 100) / 100;
-            }
-        }
-        return num
-    }
-    
-    
-    function validate_eq(){
-        if(equation.value.match(valid_eq_re)){
-            equation.style.border = "4px solid green";
-            show_graph_btn.disabled = false;
-            show_graph_btn.style.cursor = "arrow";
-            set_value();
-        }else{
-            equation.style.border = "4px solid red";
-            show_graph_btn.disabled = true;
-            show_graph_btn.style.cursor = "pointer";
-            label.map(l => l.textContent = "???");
-        }        
-    }
-    
-    
-    function set_value(){
-        const parabola = Parabola_parts(equation.value);
-        const value = [parabola.getOpening(), parabola.getVertexForm()].concat(parabola.getOtherParts());
-        
-        for(let i = 0; i < label.length; i++){
-            label[i].textContent = value[i];
-        }
-    }
-    
-    
-    function clear(){
-        equation.value = '';
-        label.map(l => l.textContent = "???");
-        show_graph_btn.disabled = true;
-        show_graph_btn.style.cursor = "pointer";
-    }
-    
-    
-    function show_graph(){
-        if(!graph_exist){
-            var elt = document.getElementById('calculator');
-            calculator = Desmos.GraphingCalculator(elt);
-            graph_exist = true;
-        }
-    
-        calculator.setExpression({ id: 'graph1', latex: equation.value});
-        main_container.style.filter = "blur(5px)";
-        graph_div.style.transform = "translate(-50%, -50%)";
-    }
-
-    
-    function hide_graph(){
-        graph_div.style.transform = "translate(-50%, -400%)";
-        main_container.style.filter = "blur(0px)";
-    }
-
-    function output_change_format(e){
-        let rl_parabola = real_life_prob();
-        if(e.target.textContent === 'To Decimal'){
-            e.target.textContent = 'To Fraction'   
-        }else{
-            e.target.textContent = 'To Decimal'
-        }
-        set_value();
-        rl_parabola.check_inputs();
-    }
-
-
-    function addEvent(){
-        clear_btn.addEventListener('click', clear);
-        equation.addEventListener('keyup', validate_eq);
-        show_graph_btn.addEventListener('click', show_graph);
-        toDecimal_btn.addEventListener('click', output_change_format.bind(event));
-        cancel_graph_btn.addEventListener('click', hide_graph);
-    }
-
-    return {
-        addEvent,
-        convert_num,
-        validate_eq,
-    }
-};
-
-
-
-
-const real_life_prob = function (){
-    const calc_inputs_btn = document.querySelector('#calculate-inputs')
-    const clear_inputs_btn = document.querySelector('#clear-inputs');
-    const rl_inputs = Array.from(document.querySelectorAll('.rl-input'));
-    rl_inputs[rl_inputs.length -1].disabled = true;
-
-    let [wide, deep, p, focus] = rl_inputs;
-    let right_side;
-    let left_side;
-    let result;
-    let x = parseFloat(wide.value) / 2;
-
-    const main_parabola = Main();
-
-    function setP(){
-        right_side = 4 * eval(deep.value);
-        result = x**2 / right_side
-        p.value = main_parabola.convert_num(parseFloat(result));
-    }
-
-
-    function setFocus(){
-        focus.value = '(0, ' + p.value + ')';
-    }
-
-
-    function setDeep(){
-        left_side = x**2;
-        right_side = parseFloat(4 * eval(p.value));
-        deep.value = main_parabola.convert_num(left_side/right_side);
-    }
-
-
-    function setWide(){
-        wide.value = main_parabola.convert_num((Math.sqrt(4*eval(p.value)*eval(deep.value)) * 2));
-    }
-
-    
-    function check_inputs(){
-        if(rl_inputs.filter(input => input.value === '').length != 2) return;
-
-        rl_inputs.forEach(input => {
-            input.disabled = true;
-            if(input.value ==- '') input.style.color = 'rgb(151, 233, 113)';
-        });
-
-        let rl_prabola = real_life_prob();
-    
-        if(p.value === '' && (wide.value != '' && deep.value != '')){
-            rl_prabola.setP();
-            rl_prabola.setFocus();
-        }else if(wide.value === '' && (deep.value != '' && p.value != '')){
-            rl_prabola.setWide();
-            rl_prabola.setFocus();
-        }else if(deep.value === '' && (wide.value != '' && p.value != '')){
-            rl_prabola.setDeep();
-            rl_prabola.setFocus();
-        }else if(p.value != '' && focus.value ===''){
-            rl_prabola.setFocus();
-        }
-    }
-
-    
-    function clear_input(){
-        rl_inputs.forEach(input => {
-            input.value = '';
-            input.disabled = false;
-            input.style.color = 'white';
-        });
-        rl_inputs[rl_inputs.length -1].disabled = true;
-    }
-
-
-    function addEvent(){
-        clear_inputs_btn.addEventListener('click', clear_input);
-        calc_inputs_btn.addEventListener('click', check_inputs);
-    }
-
-    return{
-        setP,
-        setFocus,
-        setWide,
-        setDeep,
-        check_inputs,
-        clear_input,
-        addEvent,
-    }
-};
-
-
-(function () {
-    const main_parabola = Main();
-    const rl_parabola = real_life_prob();
-
-    
-    function addEvent(){
-        main_parabola.addEvent();
-        rl_parabola.addEvent();
-        window.addEventListener('load', () => {
-            main_parabola.validate_eq();
-            rl_parabola.clear_input();
-        });
-    }
-    
-    addEvent();
-})()
+}
